f9c916e0f083771085a952a67c10d929ca9705fd
ankit.sinha <ankit.sin@media.net>
2018-04-27 18:47:32 +0530

    Use Html webpack plugin to automatically add all the output js files to the index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -7,5 +8,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './dist/index.html' })
+    ]
 }
