a7babf186c9b78cb5c82c32d6c93ee6eeaf6ff97
ankit.sinha <ankit.sin@media.net>
2018-05-09 13:06:30 +0530

    Use redux methods to update a component.  Create a simple reducer.

--- Readme.md
+++ Readme.md
@@ -16,3 +16,5 @@ This is causing one issue. When we are on some route say /about and we refresh t
 19. Used Uglijy Js Plugin to minify the production javascript build. Reference: https://github.com/mishoo/UglifyJS2/tree/harmony. The defaults options are well suited so i didn't pass any parameters to the plugin. This plugin does a lot of optimization to the code.
 
 20. Used Define plugin to define environment. The DefinePlugin allows you to create global constants which can be configured at compile time. This can be useful for allowing different behavior between development builds and release builds.
+
+21. Used redux methods to create a store, subscribe to it and update the component on change. Created a simple reducer for the same.
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createStore } from 'redux';
 
-const Home = () =>
-    <div> Home </div>
+const counter = (state = 0, action) => {
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+
+    }
+}
+
+const store = createStore(counter);
+
+
+class Home extends Component {
+    componentDidMount() {
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        });
+    }
+
+    componentWillUnmount() {
+        store.dispatch({
+            type: 'DECREMENT'
+        })
+        this.unsubscribe();
+    }
+
+    render() {
+        return <div onClick={() => {
+            store.dispatch({
+                type: 'INCREMENT'
+            })
+        }}> {store.getState()} </div>
+    }
+}
 
 
 module.exports = Home;
